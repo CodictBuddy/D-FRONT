cfe420339a3c6e6e5e360413b09b34e1bc4d479f
raunak-io <fullstackerr@gmail.com>
2021-12-26 23:18:25 +0530

    user post and comment section ui creation

---
+++ android/app/src/main/java/com/mobile/dtalks/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mobile.dtalks;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/app/home/card/card.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
+import Speech from 'speak-tts';
+
+@Component({
+  selector: 'post-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+})
+export class CardComponent implements OnInit {
+  isLiked: Boolean = false;
+  playSound: Boolean = false;
+
+  speech: any;
+  speechData: any;
+  constructor(private tts: TextToSpeech) {}
+
+  ngOnInit() {}
+
+  toggleLike() {
+    this.isLiked = !this.isLiked;
+  }
+
+  sound() {
+    this.playSound = !this.playSound;
+
+    if (!this.playSound) {
+      // this.speech.pause();
+      this.tts.stop()
+    }
+    if (this.playSound) {
+      this.tts
+        .speak({ text: 'your text reading software is working now',
+          locale: 'en-GB',
+          rate: 0.75})
+        .then(() => {
+          console.log('Success !');
+        })
+        .catch((e) => {
+          console.error('An error occurred :', e);
+        });
+    }
+  }
+
+  setLanguage(i) {
+    console.log(i);
+    console.log(
+      this.speechData.voices[i].lang + this.speechData.voices[i].name
+    );
+    this.speech.setLanguage(this.speechData.voices[i].lang);
+    this.speech.setVoice(this.speechData.voices[i].name);
+  }
+}
