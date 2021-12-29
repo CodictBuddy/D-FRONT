1d4611fd531f6bf8fe25358dcb5db38cd2639835
raunak-io <fullstackerr@gmail.com>
2021-12-29 21:41:09 +0530

    ui screens creation

---
+++ src/app/reset-password-verification/reset-password-verification.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-password-verification',
+  templateUrl: './reset-password-verification.page.html',
+  styleUrls: ['./reset-password-verification.page.scss'],
+})
+export class ResetPasswordVerificationPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
