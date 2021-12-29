cc2fc1fb17b3cb92572ebbda54564d4344825e68
raunak-io <fullstackerr@gmail.com>
2021-12-29 21:41:09 +0530

    ui screens creation

---
+++ src/app/profile-manage/profile-manage.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-manage',
+  templateUrl: './profile-manage.page.html',
+  styleUrls: ['./profile-manage.page.scss'],
+})
+export class ProfileManagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
