060ef8c057a9278d02e95ae5b988ce85bee41cad
raunak-io <fullstackerr@gmail.com>
2022-02-13 19:43:51 +0530

    user sign up login flow completion

--- src/app/complete-profile/complete-profile.page.ts
+++ src/app/complete-profile/complete-profile.page.ts
@@ -1,4 +1,8 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { UtilService } from '../utils/util.service';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-complete-profile',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./complete-profile.page.scss'],
 })
 export class CompleteProfilePage implements OnInit {
-
-  constructor() { }
+  completeProfileForm: FormGroup;
+  constructor(
+    private completeProfileBuilder: FormBuilder,
+    private util: UtilService,
+    private auth: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.completeProfileFormBuilder();
   }
 
+  completeProfileFormBuilder() {
+    this.completeProfileForm = this.completeProfileBuilder.group({
+      user_gender: ['Male', [Validators.required]],
+      user_about: ['', [Validators.required]],
+      user_headline: ['', [Validators.required]],
+      user_dob: [''],
+    });
+  }
+
+  completeProfile(valid, value) {
+    if (valid) {
+      this.auth
+        .updateProfile({ ...value, ...this.util.default_language })
+        .subscribe(
+          (res) => {
+            this.util.setLocalStorage('user_data',res.user)
+            this.util.setLocalStorage('access_token',res.access_token)
+            this.router.navigateByUrl('/profile-image');
+          },
+          (err) => {
+            console.log('complete profile Error', err);
+
+            this.util.toast(err.error['message'], 2000);
+          }
+        );
+    }
+  }
 }
