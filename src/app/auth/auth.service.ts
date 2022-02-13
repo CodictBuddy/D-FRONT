2b57c51b69c31b3e0d47313d882364e162a9b5de
raunak-io <fullstackerr@gmail.com>
2022-02-13 19:43:51 +0530

    user sign up login flow completion

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,37 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   baseurl = environment.base_url;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(_input) {
     return this.http.post<any>(this.baseurl + `/auth/login`, _input);
+  }
+
+  register(_input) {
+    return this.http.post<any>(this.baseurl + `/user/signup`, _input);
+  }
+
+  updateProfile(_input) {
+    return this.http.patch<any>(this.baseurl + `/user`, _input);
+  }
+
+  emailVarification(_input) {
+    return this.http.post<any>(this.baseurl + `/user/verify/email`, _input);
+  }
+
+  forgotPassword(_input) {
+    return this.http.post<any>(this.baseurl + `/user/forgotPassword`, _input);
+  }
+
+  resendCode(_input) {
+    return this.http.post<any>(this.baseurl + `/user/resend/code`, _input);
+  }
+
+  setNewPassword(_input) {
+    return this.http.post<any>(this.baseurl + `/user/resetPassword`, _input);
   }
 }
--- src/app/forgot-password/forgot-password.page.ts
+++ src/app/forgot-password/forgot-password.page.ts
@@ -1,4 +1,8 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { UtilService } from '../utils/util.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.page.scss'],
 })
 export class ForgotPasswordPage implements OnInit {
-
-  constructor() { }
+  emailForm: FormGroup;
+  constructor(
+    private auth: AuthService,
+    private util: UtilService,
+    private router: Router,
+    private emailFormBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.forgotPassForm();
   }
 
+  forgotPassForm() {
+    this.emailForm = this.emailFormBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+        ],
+      ],
+    });
+  }
+
+  forgotPassword(valid, value) {
+    if (valid) {
+      this.auth.forgotPassword(value).subscribe(
+        (res) => {
+          localStorage.setItem('email', value.email);
+          localStorage.setItem('_id', res._id);
+          localStorage.setItem('type', 'forgot_password');
+          this.router.navigateByUrl('/reset-password');
+        },
+        (err) => {
+          console.log('forgot password  Error', err);
+
+          this.util.toast(err.error['message'], 2000);
+        }
+      );
+    }
+  }
 }
