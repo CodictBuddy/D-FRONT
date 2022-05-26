f97cdbaca2282857f46fb42fe978fda6b646c065
raunak-io <fullstackerr@gmail.com>
2022-05-26 22:55:29 +0530

    user connections and notification implementation

---
+++ src/app/services/notification.service.ts
@@ -0,0 +1,47 @@
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  baseurl = environment.base_url;
+
+  constructor(private http: HttpClient, private socket: Socket) {
+    // this.socket.on('new-notification', () => {
+    //   console.log('i need to call notification service now');
+    //   this.getUnreadNotifications().then((res) => {
+    //     console.log('result for unread notifications here', res);
+    //   });
+    // });
+  }
+
+  getAllNotifications(queryObject) {
+    return this.http
+      .get<any>(
+        this.baseurl +
+          `/notification?skip=${queryObject.skip}&limit=${queryObject.limit}`
+      )
+      .toPromise();
+  }
+
+  getUnreadNotifications() {
+    return this.http
+      .get<any>(this.baseurl + `/notification/unread`)
+      .toPromise();
+  }
+
+  updateNotificationStatus(_input) {
+    return this.http
+      .patch<any>(this.baseurl + `/notification`, _input)
+      .toPromise();
+  }
+
+  removeNotification(id, type) {
+    return this.http
+      .delete<any>(this.baseurl + `/notification/${type}/${id}`)
+      .toPromise();
+  }
+}
--- src/app/utils/action-buttons/action-buttons.component.ts
+++ src/app/utils/action-buttons/action-buttons.component.ts
@@ -1,5 +1,6 @@
+import { ConnectionService } from './../../services/connection.service';
 import { UtilService } from '../util.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-action-buttons',
@@ -7,12 +8,160 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./action-buttons.component.scss'],
 })
 export class ActionButtonsComponent implements OnInit {
-  constructor(private util: UtilService) {}
+  constructor(
+    private util: UtilService,
+    private connectionService: ConnectionService
+  ) {}
+  @Input('user_id') user_id;
+  @Input('sender_name') sender_name;
+  @Input('sender_info') sender_info;
+
+  showBtns = false;
   multipleButtons = true;
-  ngOnInit() {}
+  connectionStatusObject = {};
+  btn1 = '';
+  btn2 = '';
+  utilButtons = this.util.connection_btns;
+  customizedActionButtonSheet = [];
+  changeBtnView = false;
+
+  ngOnInit() {
+    this.checkConnection();
+  }
+
+  async checkConnection() {
+    this.connectionStatusObject = await this.connectionDetails(
+      this.user_id,
+      this.utilButtons[0]
+    );
+
+    this.setButtonLabels(this.connectionStatusObject);
+  }
+
+  setButtonLabels(connectionStatusObject) {
+    if (!connectionStatusObject) {
+      this.btn1 = this.utilButtons[3];
+      this.btn2 = this.utilButtons[4];
+      this.showBtns = !connectionStatusObject;
+      this.multipleButtons = !connectionStatusObject;
+
+      this.customizedActionButtonSheet = this.util.modifyActionSheetOptions([
+        'Remove',
+        'Unfollow',
+      ]);
+    } else {
+      if (
+        connectionStatusObject.type === this.utilButtons[0] &&
+        connectionStatusObject.target_user_id === this.user_id
+      ) {
+        this.btn1 =
+          connectionStatusObject.connection_status === this.utilButtons[1]
+            ? this.utilButtons[1]
+            : this.utilButtons[4];
+        this.multipleButtons = !this.btn1;
+        this.showBtns = !!this.btn1;
+        this.changeBtnView = this.btn1 === this.utilButtons[4];
+
+        if (this.btn1 === this.utilButtons[1]) {
+          this.customizedActionButtonSheet = this.util.modifyActionSheetOptions(
+            ['Remove', 'Unfollow', 'Connect']
+          );
+        } else if (this.btn1 === this.utilButtons[4]) {
+          this.customizedActionButtonSheet = this.util.modifyActionSheetOptions(
+            ['Message', 'Follow', 'Connect']
+          );
+        }
+      } else if (
+        connectionStatusObject.type === this.utilButtons[5] &&
+        connectionStatusObject.target_user_id === this.user_id
+      ) {
+        this.btn1 = this.utilButtons[5];
+        this.btn2 = this.utilButtons[4];
+        this.multipleButtons = !!this.btn1;
+        this.showBtns = !!this.btn1;
+
+        this.customizedActionButtonSheet = this.util.modifyActionSheetOptions([
+          'Remove',
+          'Unfollow',
+          'Follow',
+          'Message',
+        ]);
+      }
+    }
+  }
 
   async openActionSheet() {
-    const b = this.util.alert_options;
-    await this.util.dynamicActionSheet({ buttons: b });
+    const b = this.customizedActionButtonSheet;
+    const { data } = await (
+      await this.util.dynamicActionSheet({ buttons: b })
+    ).onDidDismiss();
+    if (data) {
+      this.handleConnection(data, this.user_id);
+    }
+  }
+
+  async connectionDetails(user_id, connection_type) {
+    console.log('entered here', user_id);
+    let connectionResponse = {};
+    connectionResponse = await this.connectionService.getConnectionDetail({
+      user_id,
+      connection_type,
+    });
+
+    if (!connectionResponse && connection_type === this.utilButtons[5]) {
+      return null;
+    }
+
+    if (!connectionResponse && connection_type === this.utilButtons[0]) {
+      connectionResponse = await this.connectionDetails(
+        user_id,
+        this.utilButtons[5]
+      );
+    }
+    return connectionResponse;
+  }
+
+  handleConnection(type, user_id) {
+    if (type === this.util.connection_btns[0]) {
+      this.createConnection(
+        user_id,
+        type,
+        `${this.sender_name} ${this.util.notification_template_constants.connection_req_sent}`,
+        this.sender_name,
+        `/dashboard/${this.sender_info['_id']}`
+      );
+    } else if (type === 'Remove') {
+      this.removeConnection(user_id, this.util.connection_btns[0]);
+    }
+  }
+
+  /**
+   * handling connection api calls
+   *
+   */
+  async createConnection(
+    user_id,
+    type,
+    message,
+    invitation_title,
+    navigation_url
+  ) {
+    const res = await this.connectionService.createConnection({
+      user_id,
+      type,
+      message,
+      invitation_title,
+      navigation_url,
+    });
+    this.checkConnection();
+    return res;
+  }
+
+  removeConnection(user_id, connection_type) {
+    this.connectionService
+      .removeConnection(user_id, connection_type)
+      .then(() => {
+        this.checkConnection();
+      });
   }
 }
